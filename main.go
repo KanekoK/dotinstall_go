c0484e1d6f60ff0fb13bdb3abf646cb8d1e5e176
金子健哉 <kaneko@atsu-maru.co.jp>
2018-12-13 00:04:49 +0900

    .

--- main.go
+++ main.go
@@ -5,9 +5,10 @@ import (
 	"time"
 )
 
-func task1 () {
+func task1 (result chan string) {
 	time.Sleep(time.Second * 2)
 	fmt.Println("task1 finished!")
+	result<- "task1 result"
 }
 
 func task2 () {
@@ -15,8 +16,12 @@ func task2 () {
 }
 
 func main() {
-	go task1()
+	result := make(chan string)
+
+	go task1(result)
 	go task2()
+
+	fmt.Println(<-result)
 
 	time.Sleep(time.Second * 3)
 }
