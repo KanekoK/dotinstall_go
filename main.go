dd90cace8cf922489e2ac7dcc60d306cfa638c15
金子健哉 <kaneko@atsu-maru.co.jp>
2018-12-13 00:32:27 +0900

    .

--- main.go
+++ main.go
@@ -2,26 +2,14 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"net/httpd"
 )
 
-func task1 (result chan string) {
-	time.Sleep(time.Second * 2)
-	fmt.Println("task1 finished!")
-	result<- "task1 result"
-}
-
-func task2 () {
-	fmt.Println("task2 finished!")
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi %s!", r.URL.Path[1:])
 }
 
 func main() {
-	result := make(chan string)
-
-	go task1(result)
-	go task2()
-
-	fmt.Println(<-result)
-
-	time.Sleep(time.Second * 3)
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
