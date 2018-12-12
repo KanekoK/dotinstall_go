ed40250c0b27c6042e6db30f9d8342ace8c3cf78
金子健哉 <kaneko@atsu-maru.co.jp>
2018-12-13 00:01:31 +0900

    goroutine

--- main.go
+++ main.go
@@ -1,44 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-func show(t interface{}) {
-	// _, ok := t.(japanese)
-	// if ok {
-	// 	fmt.Println("I am Japanese.")
-	// } else {
-	// 	fmt.Println("I am not Japanese.")
-	// }
-
-	switch t.(type) {
-	case japanese:
-		fmt.Println("i am japanese")
-	default:
-		fmt.Println("i am not japanese")
-	}
-
+func task1 () {
+	time.Sleep(time.Second * 2)
+	fmt.Println("task1 finished!")
 }
 
-
-
-type greeter interface {
-	greet()
-}
-
-type japanese struct {}
-type american struct {}
-
-func (j japanese) greet() {
-	fmt.Println("Konnnichiwa!")
-}
-func (a american) greet() {
-	fmt.Println("Hello!")
+func task2 () {
+	fmt.Println("task2 finished!")
 }
 
 func main() {
-	greeters := []greeter{japanese{}, american{}}
-	for _, greeter := range greeters {
-		greeter.greet()
-		show(greeter)
-	}
+	go task1()
+	go task2()
+
+	time.Sleep(time.Second * 3)
 }
