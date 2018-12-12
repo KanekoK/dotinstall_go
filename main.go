9309d289ff0b98b31fe7fa667a696a4a2376b303
金子健哉 <kaneko@atsu-maru.co.jp>
2018-12-12 23:51:43 +0900

    インターフェースについて

--- main.go
+++ main.go
@@ -2,14 +2,23 @@ package main
 
 import "fmt"
 
+type greeter interface {
+	greet()
+}
+
+type japanese struct {}
+type american struct {}
+
+func (j japanese) greet() {
+	fmt.Println("Konnnichiwa!")
+}
+func (a american) greet() {
+	fmt.Println("Hello!")
+}
+
 func main() {
-	// const name = "taguchi"
-	// name = "fkoji"
-	// fmt.Println(name)
-	const (
-		sun = iota
-		mon
-		tue
-	)
-	fmt.Println(sun, mon, tue)
+	greeters := []greeter{japanese{}, american{}}
+	for _, greeter := range greeters {
+		greeter.greet()
+	}
 }
